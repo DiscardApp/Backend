33aa620fe42b88f396667d139365f7edca5ddd60
Mathis Mensing <matmen@dreadful.tech>
2020-03-15 17:37:53 +0100

    Index: Returns app name

--- controllers/Index.js
+++ controllers/Index.js
@@ -2,11 +2,11 @@ const ControllerContext = require('../utils/http/ControllerContext');
 
 class Index extends ControllerContext {
 	route(router) {
-		router.register('/{swag}', this);
+		router.register('/', this);
 	}
 
 	get() {
-		this.respond({ parameters: this.parameters, message: 'Tümer Türkmen approved' });
+		this.respond('DiscardApp');
 	}
 }
 
