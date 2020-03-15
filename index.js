ccc6d614a8dfe170ab5c56fb667b613a849860cc
Mathis Mensing <matmen@dreadful.tech>
2020-03-15 18:27:17 +0100

    Main: Worker threads

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+const cluster = require('cluster');
+const os = require('os');
 const ConfigManager = require('./utils/configs/ConfigManager');
 const HTTPHandler = require('./utils/http/HTTPHandler');
 
 class DiscardApp {
 
 	constructor() {
+		if (cluster.isMaster)
+			this.masterSetup();
+		else
+			this.workerSetup();
+	}
+
+	masterSetup() {
+		console.log(`[Master.${process.pid}] Starting`);
+
+		for (let i = 0; i <= os.cpus().length; i++) {
+			const worker = cluster.fork();
+			worker.on('exit', () => {
+				console.log(`[Worker.${worker.process.pid}] Worker died, restarting`);
+				cluster.fork();
+			});
+		}
+	}
+
+	workerSetup() {
+		console.log(`[Worker.${process.pid}] Starting`);
 		this.configManager = new ConfigManager();
 		this.httpHandler = new HTTPHandler(this.configManager.http);
 	}
-
 }
 
 module.exports = new DiscardApp();
