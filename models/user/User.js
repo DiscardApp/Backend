2850e54ae7ee0d82da73243a99e1cd540f099d16
Mathis Mensing <matmen@dreadful.tech>
2020-03-17 14:10:37 +0100

    User: Add password support

--- models/user/User.js
+++ models/user/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const Model = require('../../utils/model/Model');
 
 class User extends Model {
@@ -14,8 +15,33 @@ class User extends Model {
 			username: {
 				type: 'VARCHAR(32)',
 				notNull: true
+			},
+			password: {
+				type: 'VARCHAR(60)',
+				notNull: true,
+				hide: true
 			}
 		};
+	}
+
+	/**
+	 * Creates a hash of the given plain-text password
+	 * @param {string} password The plain-text password to hash
+	 * @param {number} [rounds] The rounds of salt to use
+	 * @returns {Promise<string>} The hashed password
+	 */
+	static hashPassword(password, rounds = 12) {
+		return bcrypt.hash(password, rounds);
+	}
+
+	/**
+	 * Compares a plain-text password with a hash and returns whether they match
+	 * @param {string} password The plain-text password
+	 * @param {string} hash The hashed password
+	 * @returns {Promise<boolean>} Whether the password matches
+	 */
+	static comparePassword(password, hash) {
+		return bcrypt.compare(password, hash);
 	}
 
 }
