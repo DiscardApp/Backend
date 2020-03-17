ed3483e10cb2dfcbcb74639f615c059d7b1cbcb3
Mathis Mensing <matmen@dreadful.tech>
2020-03-17 23:08:36 +0100

    Permissions: Move into User model

--- models/user/User.js
+++ models/user/User.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcrypt');
 const Model = require('../../utils/model/Model');
-const Permissions = require('./Permissions');
 
 class User extends Model {
 
@@ -77,6 +76,31 @@ class User extends Model {
 		return ['active', 'verifiedEmail', 'verifiedPhone', 'admin'];
 	}
 
+}
+
+class Permissions {
+	constructor(availablePermissions, grantedPermissions) {
+		this.permissions = grantedPermissions;
+		this.availablePermissions = availablePermissions;
+
+		for (const permission of availablePermissions) {
+			const index = availablePermissions.indexOf(permission);
+			const value = 2 ** index;
+
+			this[permission] = (grantedPermissions & value) === value;
+		}
+	}
+
+	static from(availablePermissions, grantedPermissions = []) {
+		let value = 0;
+
+		for (const name of grantedPermissions) {
+			if (!availablePermissions.includes(name)) throw new Error(`Invalid permission name ${name}`);
+			value += 2 ** availablePermissions.indexOf(name);
+		}
+
+		return value;
+	}
 }
 
 module.exports = User;
